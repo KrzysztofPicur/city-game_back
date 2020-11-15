43ead0149e2d00794ca9c2580a2c0952eb279a06
KMP <krzysztof.picur@gmail.com>
2020-11-16 00:43:36 +0100

    small fixes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\FacebookController;
+use App\Http\Controllers\GoogleController;
 
 
 /*
@@ -22,14 +22,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix' => 'auth', 'middleware' => 'api'], function () {
-    Route::post('register', [ AuthController::class, 'register']);
-    Route::post('login',    [ AuthController::class, 'login']);
-    Route::post('refresh',  [ AuthController::class, 'refresh']);
-    Route::get('profile',   [ AuthController::class, 'profile']);
-    Route::post('logout',   [ AuthController::class, 'logout']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login',    [AuthController::class, 'login']);
+    Route::post('refresh',  [AuthController::class, 'refresh']);
+    Route::get('profile',   [AuthController::class, 'profile']);
+    Route::post('logout',   [AuthController::class, 'logout']);
 });
 
-Route::get('google',          [ GoogleController::class, 'redirectToGoogle']);
-Route::get('google/callback', [ GoogleController::class, 'handleGoogleCallback']);
+Route::get('google',          [GoogleController::class, 'redirectToGoogle']);
+Route::get('google/callback', [GoogleController::class, 'handleGoogleCallback']);
 
 
