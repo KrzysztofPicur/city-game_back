036b5d25989f54d3bca25ce6ea15bc0fe5450a5a
KMP <krzysztof.picur@gmail.com>
2020-11-16 00:00:00 +0100

    adding routes for google login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FacebookController;
 
 
 /*
@@ -14,16 +15,21 @@ use App\Http\Controllers\AuthController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::group(['prefix' => 'auth', 'middleware' => 'api'], function () {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::get('profile', [AuthController::class, 'profile']);
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('register', [ AuthController::class, 'register']);
+    Route::post('login',    [ AuthController::class, 'login']);
+    Route::post('refresh',  [ AuthController::class, 'refresh']);
+    Route::get('profile',   [ AuthController::class, 'profile']);
+    Route::post('logout',   [ AuthController::class, 'logout']);
 });
+
+Route::get('google',          [ FacebookController::class, 'redirectToGoogle']);
+Route::get('google/callback', [ FacebookController::class, 'handleGoogleCallback']);
+
+
