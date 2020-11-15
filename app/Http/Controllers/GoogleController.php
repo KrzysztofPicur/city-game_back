609b0ca29cf32b47658fb6202c9729f62cf6d116
KMP <krzysztof.picur@gmail.com>
2020-11-15 23:54:41 +0100

    adding GoogleController and defined json response after login

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class GoogleController extends Controller
 {
-    //
+    public function redirectToFacebook() {
+        return Socialite::driver('google')->stateless()->redirect();
+
+    }
+
+    public function handleFacebookCallback() {
+
+        $user =  Socialite::driver('google')->stateless()->user();
+
+        $google_user_id = $user->getId();
+        $name           = $user->getName();
+        $avatar         = $user->getAvatar();
+        $email          = $user->getEmail();
+        $token          = $user->token;
+
+
+        $user = User::where([
+            'name'           => $name,
+            'token'          => $token,
+            'email'          => $email,
+            'google_user_id' => $google_user_id
+        ])->first();
+
+
+        $user = User::firstOrCreate(
+            ['name'           => $name ],
+            ['email'          => $email],
+            ['google_user_id' => $google_user_id]
+        );
+
+
+        return response()->json([
+            'access_token' => $token,
+            'name'         => $name,
+            'email'        => $email,
+            'avatar'       => $avatar,
+        ]);
+    }
 }
