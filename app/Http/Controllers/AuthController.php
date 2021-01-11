95fd6e84424d0094477bda820b9a3de7f35c74a3
kmp <krzysztof.picur@gmail.com>
2021-01-11 20:34:40 +0100

    some auth fixes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,6 +28,8 @@ class AuthController extends Controller
      */
     public function register(Request $request)
     {
+
+
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
